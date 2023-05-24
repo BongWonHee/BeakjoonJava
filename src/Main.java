5533e5e5acfc2a6a23b78567208b242beae73835
BongWonHee <leeban1004@gmail.com>
2023-05-24 19:50:38 +0900

    집

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
 
+import javax.naming.event.NamingExceptionEvent;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner taja = new Scanner(System.in);
 
-       
+        int a, b;
+        a = taja.nextInt();
+        b = taja.nextInt();
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        if (a > b) {
+            System.out.println(">");
+
+        } 
+        if (a < b)
+        {
+            System.out.println("<");
+        }
+        if (a == b)
+            
+        {
+            System.out.println("==");
+        }
 
         // return 0;
 
     }
 }
-
-
