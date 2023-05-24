669fa3e2b3be185679dc396e6f684699f1f306c8
BongWonHee <leeban1004@gmail.com>
2023-05-24 18:05:14 +0900

    go

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner taja = new Scanner(System.in);
+
        
-        int a ;
-        int b ; 
-        int c ;
-        
 
-        a = taja.nextInt();
-        b = taja.nextInt();
-        c = taja.nextInt();
-
-        System.out.println(a + b % c);
-        System.out.println(a - b % c);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
         // return 0;
 
     }
 }
+
+
