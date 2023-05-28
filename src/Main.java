e9460bc1a3e7db81c1cea33a1711db785f5601d0
BongWonHee <leeban1004@gmail.com>
2023-05-28 09:34:10 +0900

    굿모닝

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,28 @@ public class Main {
 
         Scanner taja = new Scanner(System.in);
 
-        int h, m, s;
+        int a, b, c;
         
-        h = taja.nextInt();
-        m = taja.nextInt();
-        s = taja.nextInt();
+        a = taja.nextInt();
+        b = taja.nextInt();
+        c = taja.nextInt();
 
-       int min = h * 60 + m;
-       min += s;
+     if(a == b && b == c){
 
-       int hour = (min/60)%24;
-
-       int mint = min%60;
-
-        System.out.println(hour + " " + mint);
+        System.out.println( 10000 + (a * 1000) );
+     }else if ( a == b ){
+        System.out.println(1000 + (a * 100));
+     }else if ( b == c){
+        System.out.println(1000 + ( b * 100));
+     }else if ( c == a){
+        System.out.println( 1000 + (c * 100));
+     }   
+     else{
+        int maxiem = Math.max(a, Math.max(b,c));
+        System.out.println(maxiem*100);
+    
+     }
+    
 
     }
 }
