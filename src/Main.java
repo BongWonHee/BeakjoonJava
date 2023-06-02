621dfa4981ee16e3da302f9f1de1221432d2cfba
BongWonHee <leeban1004@gmail.com>
2023-06-02 18:20:40 +0900

    집에서 계속

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    public static <DataTimeFormatter> void main(String[] args) {
 
-        Scanner taja = new Scanner(System.in);
+   // 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
 
-        int a, b, c;
-        
-        a = taja.nextInt();
-        b = taja.nextInt();
-        c = taja.nextInt();
+   // 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다
 
-     if(a == b && b == c){
 
-        System.out.println( 10000 + (a * 1000) );
-     }else if ( a == b ){
-        System.out.println(1000 + (a * 100));
-     }else if ( b == c){
-        System.out.println(1000 + ( b * 100));
-     }else if ( c == a){
-        System.out.println( 1000 + (c * 100));
-     }   
-     else{
-        int maxiem = Math.max(a, Math.max(b,c));
-        System.out.println(maxiem*100);
-    
-     }
-    
+   public static <DataTimeFormatter> void main(String[] args) {
 
-    }
-}
+      Scanner taja = new Scanner(System.in);
+
+      int add = taja.nextInt(); // 수열 크기 입력
+      int[] num = new int[add];
+      int v = taja.nextInt();
+      for (int i = 0; i < num.length; i++) {
+         num[i] = taja.nextInt();
+
+      }for (int j = 0; j < num.length; j++) {
+         
+         int max = num[0];
+         int min = num[0];
+         max = taja.nextInt();
+         min = taja.nextInt();
+
+         if(num[j] > max){
+            max = num[j];
+         }if(num[j] < min){
+            min = num[j];
+         }System.out.println(max + " " + min);
+      }
+       taja.close(); 
+      
+      }
+   }
