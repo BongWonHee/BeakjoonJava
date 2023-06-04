ca195f5291b4cb5fbd7b0ef74cf26087a525d470
BongWonHee <leeban1004@gmail.com>
2023-06-04 18:44:56 +0900

    z

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static <DataTimeFormatter> void main(String[] args) {
+   public static void main(String[] args) {
 
-        Scanner taja = new Scanner(System.in);
+      Scanner taja = new Scanner(System.in);
 
-        int a, b, c;
-        
-        a = taja.nextInt();
-        b = taja.nextInt();
-        c = taja.nextInt();
+      int a, b;
 
-     if(a == b && b == c){
+      while (taja.hasNextInt()) {
 
-        System.out.println( 10000 + (a * 1000) );
-     }else if ( a == b ){
-        System.out.println(1000 + (a * 100));
-     }else if ( b == c){
-        System.out.println(1000 + ( b * 100));
-     }else if ( c == a){
-        System.out.println( 1000 + (c * 100));
-     }   
-     else{
-        int maxiem = Math.max(a, Math.max(b,c));
-        System.out.println(maxiem*100);
-    
-     }
-    
+         a = taja.nextInt();
+         b = taja.nextInt();
 
-    }
+         System.out.println(a + b);
+
+      }
+      taja.close();
+   }
+
 }
